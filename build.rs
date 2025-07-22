d5b20210762e6501a5f0fcd149ea24ad0953f6a3
Speedy_Lex <78314533+speedy-lex@users.noreply.github.com>
2025-07-22 22:29:27 +0200

    fix build

--- build.rs
+++ build.rs
@@ -2,6 +2,27 @@ use std::env;
 use std::process::Command;
 use std::path::PathBuf;
 
+fn opt_level_to_zig(opt_level: &str) -> &str {
+    match opt_level {
+        "0" => "Debug",
+        "1" | "2" => "ReleaseSafe",
+        "3" => "ReleaseFast",
+        "s" | "z" => "ReleaseSmall",
+        _ => "Debug", // fallback if unknown
+    }
+}
+
+fn target_to_zig(target: &str) -> &str {
+    match target {
+        "x86_64-pc-windows-msvc" => "x86_64-windows-msvc",
+        "x86_64-unknown-linux-gnu" => "x86_64-linux-gnu",
+        "aarch64-apple-darwin" => "aarch64-macos",
+        "wasm32-unknown-unknown" => "wasm32-freestanding",
+        // TODO: more mappings
+        _ => target,
+    }
+}
+
 fn main() {
     let neonucleus_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("neonucleus");
 
@@ -9,6 +30,9 @@ fn main() {
         .arg("build")
         .arg("engine")
         .arg("-DnoEmu")
+        .arg("-Dbaremetal")
+        .arg(format!("-Dtarget={}", target_to_zig(&env::var("TARGET").unwrap())))
+        .arg(format!("-Doptimize={}", opt_level_to_zig(&env::var("OPT_LEVEL").unwrap())))
         .current_dir(&neonucleus_dir)
         .status()
         .expect("Failed to run zig build");
